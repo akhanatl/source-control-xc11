42f6bcf917f3f900d7d4173c067caa9b3a63cf8b
Arsal Khan <akhan45@student.gsu.edu>
2020-07-15 18:57:27 -0400

    Initial Commit

---
+++ Source Control/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Source Control
+//
+//  Created by Arsal Khan on 7/15/20.
+//  Copyright © 2020 GSU Computer Science. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Arsal Khan on 7/15/20.
+//  Copyright © 2020 GSU Computer Science. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
